5fe45265a7a50623b7606dfc67398e44709ed8b4
mborchers <mborchers@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2003-01-05 19:14:12 +0000

    initial checkin for i2c-dev-module

---
+++ src/i2c-dev.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _I2C_DEV_H
+#define _I2C_DEV_H
+
+typedef struct _I2CDEV_DATA {
+    int number_ga;
+    int number_gl;
+    int number_fb;
+		int ga_min_active_time;
+} I2CDEV_DATA;
+
+void readconfig_I2C_DEV(xmlDocPtr doc, xmlNodePtr node, int busnumber);
+int init_lineI2C_DEV(int );
+int init_bus_I2C_DEV(int );
+int term_bus_I2C_DEV(int );
+int getDescription_I2C_DEV(char *reply);
+void* thr_sendrec_I2C_DEV(void *);
+
+#endif
