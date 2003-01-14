6cc0e658ae79282855728d026cb9c7c18da3f29f
mtrute <mtrute@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2003-01-14 17:01:52 +0000

    Timestamps und Formatierungen

--- trunk/srcpd/src/srcp-error.c
+++ trunk/srcpd/src/srcp-error.c
@@ -9,87 +9,87 @@
 
 #include "srcp-error.h"
 
-int srcp_fmt_msg(int errorcode, char *msg)
+int srcp_fmt_msg(int errorcode, char *msg, struct timeval time)
 {
   switch (errorcode)
   {
     case 100:
-      sprintf(msg, "%d INFO", errorcode);
+      sprintf(msg, "%lu.%lu %d INFO", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 101:
-      sprintf(msg, "%d TERM", errorcode);
+      sprintf(msg, "%lu.%lu %d TERM", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 102:
-      sprintf(msg, "%d INIT", errorcode);
+      sprintf(msg, "%lu.%lu %d INIT", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 110:
-      sprintf(msg, "%d INFO", errorcode);
+      sprintf(msg, "%lu.%lu %d INFO", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 200:
-      sprintf(msg, "%d OK", errorcode);
-      break;
-    case 201:
-      sprintf(msg, "%d OK CONNECTION MODE", errorcode);
+      sprintf(msg, "%lu.%lu %d OK\n", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 202:
-      sprintf(msg, "%d OK PROTOCOL SRCP", errorcode);
+      sprintf(msg, "%lu.%lu %d OK CONNECTION MODE\n", time.tv_sec, time.tv_usec/1000, errorcode);
+      break;
+    case 201:
+      sprintf(msg, "%lu.%lu %d OK PROTOCOL SRCP\n", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 400:
-        sprintf(msg, "%d ERROR unsupported protocol", errorcode);
+        sprintf(msg, "%lu.%lu %d ERROR unsupported protocol", time.tv_sec, time.tv_usec/1000, errorcode);
         break;
     case 401:
-        sprintf(msg, "%d ERROR unsupported connectionmode", errorcode);
+        sprintf(msg, "%lu.%lu %d ERROR unsupported connectionmode", time.tv_sec, time.tv_usec/1000, errorcode);
         break;
     case 402:
-        sprintf(msg, "%d ERROR unsufficient data", errorcode);
+        sprintf(msg, "%lu.%lu %d ERROR unsufficient data", time.tv_sec, time.tv_usec/1000, errorcode);
         break;
     case 410:
-      sprintf(msg, "%d ERROR unknown command", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unknown command\n", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 411:
-      sprintf(msg, "%d ERROR unknown value", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unknown value", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 412:
-      sprintf(msg, "%d ERROR wrong value", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR wrong value", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 413:
-      sprintf(msg, "%d ERROR temporarily prohibited", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR temporarily prohibited", time.tv_sec, time.tv_usec/1000, errorcode);
        break;
     case 414:
-      sprintf(msg, "%d ERROR device locked", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR device locked", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 415:
-      sprintf(msg, "%d ERROR forbidden", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR forbidden", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 416:
-      sprintf(msg, "%d ERROR no data", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR no data", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 417:
-      sprintf(msg, "%d ERROR timeout", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR timeout", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 418:
-      sprintf(msg, "%d ERROR list too long", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR list too long", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 419:
-      sprintf(msg, "%d ERROR list too short", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR list too short", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 420:
-      sprintf(msg, "%d ERROR unsupported device protocol", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unsupported device protocol", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 421:
-      sprintf(msg, "%d ERROR unsupported device", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unsupported device", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 422:
-      sprintf(msg, "%d ERROR unsupported device group", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unsupported device group", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 423:
-      sprintf(msg, "%d ERROR unsupported operation", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR unsupported operation", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     case 424:
-      sprintf(msg, "%d ERROR device reinitialized", errorcode);
+      sprintf(msg, "%lu.%lu %d ERROR device reinitialized", time.tv_sec, time.tv_usec/1000, errorcode);
       break;
     default:
-      sprintf(msg, "600 ERROR internal error %d, please report to srcpd-devel@srcpd.sorceforge.net", errorcode);
+      sprintf(msg, "%lu.%lu 600 ERROR internal error %d, please report to srcpd-devel@srcpd.sorceforge.net", time.tv_sec, time.tv_usec/1000, errorcode);
       return 1;
   }
   return 0;
