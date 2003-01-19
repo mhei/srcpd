2df16eeed226eca16949e5098567f1d4a88e2c27
sieloff <sieloff@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2003-01-19 13:33:18 +0000

    $id$ inserted, // comment at last line, to avoid compiler warnings, #define  _SRCP_MASTER_INCLUDE_H to avoid multiple includes

--- src/stdincludes.h
+++ src/stdincludes.h
@@ -1,10 +1,21 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
 /*
  * Hauptinclude Datei fuer alle Sourcefiles
  *
  * V1.0 01/05/03 MAM
+ *
  */
 
 #ifndef _SRCP_MASTER_INCLUDE_H
+#define  _SRCP_MASTER_INCLUDE_H
 
 #include <stdio.h>
 
@@ -95,4 +106,4 @@
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
-#endif _SRCP_MASTER_INCLUDE_H
+#endif         // _SRCP_MASTER_INCLUDE_H
