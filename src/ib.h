3134d7d76a2849c42e000dbf071657546de14634
schmischi <schmischi@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2004-02-03 00:20:55 +0000

    some definitions

--- src/ib.h
+++ src/ib.h
@@ -48,4 +48,59 @@ void send_command_sm_ib(int busnumber);
 void check_status_ib(int busnumber);
 void check_status_pt_ib(int busnumber);
 
+
+#define XLok        0x80
+#define XLokSts     0x84
+#define XLokCfg     0x85
+#define XFunc       0x88
+#define XFuncX      0x89
+#define XFuncSts    0x8C
+#define XFuncXSts   0x8D
+#define XTrnt       0x90
+#define XTrntFree   0x93
+#define XTrntSts    0x94
+#define XTrntGrp    0x95
+#define XSensor     0x98
+#define XSensOff    0x99
+#define X88PGet     0x9C
+#define X88PSet     0x9D
+#define Xs88PTim    0x9E
+#define Xs88Cnt     0x9F
+#define XVer        0xA0
+#define XP50C       0xA1
+#define XStatus     0xA2
+#define XSOGet      0xA4
+#define XHalt       0xA5
+#define XPwrOff     0xA6
+#define XPwrOn      0xA7
+#define XLokoNet    0xC0
+#define XNOP        0xC4
+#define XDummy      0xC4
+#define XP50Len1    0xC6
+#define XP50Len2    0xC7
+#define XEvent      0xC8
+#define XEvtLok     0xC9
+#define XEvtTrn     0xCA
+#define XEvtSen     0xCB
+#define XEvtIR      0xCC
+#define XEvtTkR     0xCF
+#define XEvtMem     0xD0
+#define XCR         0x0D
+
+// fehlercodes der IB
+
+#define XBADPARAM   0x02
+#define XPWOFF      0x06
+#define XNOTSPC     0x07
+#define XNOLSPC     0x08
+#define XNODATA     0x0A
+#define XNOSLOT     0x0B
+#define XBADLNP     0x0C
+#define XBADTNP     0x0E
+#define XLKBUSY     0x0D
+#define XNOISPC     0x10
+#define XLOWTSP     0x40
+#define XLKHALT     0x41
+#define XLKPOFF     0x42
+
 #endif
