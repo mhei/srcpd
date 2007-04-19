8c6a44a3ac17bfc43d8260d0baddf3ca99fa9450
gvandersel <gvandersel@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2007-04-19 17:18:55 +0000

    SLX852 added

--- src/selectrix.h
+++ src/selectrix.h
@@ -15,22 +15,40 @@
 #define SXwrite     0x80    /* Schreib befehl fur Selectrixbus */
 
 /* Maximum number off adresses on the SX-bus */
-#define SXmax     112   /* Number of adresses on the SX-bus*/
+#define SXmax       112   /* Number of adresses on the SX-bus*/
 #define SXcc2000    104   /* Number of adresses on the SX-bus with a CC-2000 */
 
 /* Adressses on the SX_bus */
-#define SXcontrol   127   /* Control adres (all SX controllers) */
-#define SXstatus    109   /* Status adres (CC-2000) */
-#define SXcommand   106   /* Command adres (CC-2000) */
-#define SXprog2     105   /* Program parameter 2 (CC-2000) */
-#define SXporg1     104   /* Program parameter 1 (CC-2000) */
+#define SXcontrol   127   /* Power on/off adres (all SX controllers) */
+#define RautenhsCC  126   /* Rautenhaus control address */
 
+/* Control adddresses for a CC2000 */
+#define SXstatus    109   /* Status adres */
+#define SXcommand   106   /* Command adres */
+#define SXprog2     105   /* Program parameter 2 */
+#define SXporg1     104   /* Program parameter 1 */
+
+/* Rautenhuas control bits (for adres 126) */
+#define cntrlON     128   /* Start Rautenhaus protocol */
+#define cntrlOFF     64   /* Stop Rautenhaus protocol */
+#define fdbckON      32   /* Changes on then SX bus are reported automaticaly */
+#define fdbckOFF     16   /* Stop reporting changes on the SX bus */
+#define clkOFF0       8   /* Don't check adres 111 (clock) on bus 1 */
+#define clkOFF1       4   /* Don't check adres 111 (clock) on bus 0 */
+
+/* Constants for flags */
 /* Central is a special */
-#define CC2000_MODE   0x0001    /* CC2000 has restricted bus space and can program decoders */
-#define Rautenhause_MODE   0x0002    /* Rautenhaus has special feedback support */
+#define CC2000_MODE       0x0001    /* CC2000 has restricted bus space and can program decoders */
+#define Rautenhaus_MODE   0x0002    /* Rautenhaus has special feedback support */
+
+#define Rautenhaus_FDBCK  0x0010    /* Automatic feedback report */
+#define Rautenhaus_DBL    0x0020    /* Two busses are controlled */
+#define Rautenhaus_ADR    0x0040    /* Last byte was an address */
 
 /* Array with the size of an SX-bus */
-typedef int SX_BUS[SXmax];
+/* Array with the size of two SX-busses  */
+//typedef int SX_BUS[SXmax];
+typedef int SX_BUS[256];     /* Space for adresses/data on the SX-bus */
 
 /* Structure of a Selectrix bus */
 typedef struct _SELECTRIX_DATA {
