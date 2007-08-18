7493fc46a0b3ad4ef3056e962dfa007f27bbbb60
gscholz <gscholz@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2007-08-18 08:57:42 +0000

    change power state change again

--- trunk/srcpd/src/loopback.c
+++ trunk/srcpd/src/loopback.c
@@ -197,9 +197,9 @@ void *thr_sendrec_LOOPBACK(void *v)
 
     while (1) {
         if (busses[bus].power_changed == 1) {
+            busses[bus].power_changed = 0;
             /*
             char msg[110];
-            busses[bus].power_changed = 0;
             infoPower(bus, msg);
             queueInfoMessage(msg);
             */
