e1aabf7848e8c03c998d3ee2131043d07c93b266
gscholz <gscholz@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2007-11-19 16:51:11 +0000

    smarter init.d messages using debian lsb templates, »reload« is enabled but currently not working (be patient)

--- debian/init.d
+++ debian/init.d
@@ -1,13 +1,13 @@
 #! /bin/sh
 #
-# skeleton	example file to build /etc/init.d/ scripts.
-#		This file should be used to construct scripts for /etc/init.d.
+# skeleton      example file to build /etc/init.d/ scripts.
+#               This file should be used to construct scripts for /etc/init.d.
 #
-#		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
-#		Modified for Debian 
-#		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
+#               Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+#               Modified for Debian 
+#               by Ian Murdock <imurdock@gnu.ai.mit.edu>.
 #
-# Version:	@(#)skeleton  1.9  26-Feb-2001  miquels@cistron.nl
+# Version:      @(#)skeleton  1.9  26-Feb-2001  miquels@cistron.nl
 #
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
@@ -19,56 +19,61 @@ test -x $DAEMON || exit 0
 
 # Include srcpd defaults if available
 if [ -f /etc/default/srcpd ] ; then
-	. /etc/default/srcpd
+        . /etc/default/srcpd
 fi
+
+# Get lsb functions
+. /lib/lsb/init-functions
+. /etc/default/rcS
 
 set -e
 
 case "$1" in
   start)
-	echo -n "Starting $DESC: "
-	start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON -- $DAEMON_OPTS
-	echo "$NAME."
-	;;
+        log_begin_msg "Starting $DESC..."
+        start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
+                --exec $DAEMON -- $DAEMON_OPTS
+        log_end_msg $?
+        ;;
   stop)
-	echo -n "Stopping $DESC: "
-	start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON
-	echo "$NAME."
-	;;
-  #reload)
-	#
-	#	If the daemon can reload its config files on the fly
-	#	for example by sending it SIGHUP, do it here.
-	#
-	#	If the daemon responds to changes in its config file
-	#	directly anyway, make this a do-nothing entry.
-	#
-	# echo "Reloading $DESC configuration files."
-	# start-stop-daemon --stop --signal 1 --quiet --pidfile \
-	#	/var/run/$NAME.pid --exec $DAEMON
-  #;;
+        log_begin_msg "Stopping $DESC..."
+        start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
+                --exec $DAEMON
+        log_end_msg $?
+        ;;
+  reload)
+        #       If the daemon can reload its config files on the fly
+        #       for example by sending it SIGHUP, do it here.
+        #
+        #       If the daemon responds to changes in its config file
+        #       directly anyway, make this a do-nothing entry.
+
+        log_begin_msg "Reloading $DESC configuration files..."
+        start-stop-daemon --stop --signal 1 --quiet --pidfile \
+                /var/run/$NAME.pid --exec $DAEMON
+        log_end_msg $?
+  ;;
   restart|force-reload)
-	#
-	#	If the "reload" option is implemented, move the "force-reload"
-	#	option to the "reload" entry above. If not, "force-reload" is
-	#	just the same as "restart".
-	#
-	echo -n "Restarting $DESC: "
-	start-stop-daemon --stop --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON
-	sleep 2
-	start-stop-daemon --start --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS
-	echo "$NAME."
-	;;
+        #
+        #       If the "reload" option is implemented, move the "force-reload"
+        #       option to the "reload" entry above. If not, "force-reload" is
+        #       just the same as "restart".
+        #
+        log_begin_msg "Restarting $DESC..."
+        start-stop-daemon --stop --quiet --pidfile \
+                /var/run/$NAME.pid --exec $DAEMON
+        sleep 2
+        start-stop-daemon --start --quiet --pidfile \
+                /var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS
+        log_end_msg $?
+        ;;
   *)
-	N=/etc/init.d/$NAME
-	# echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $N {start|stop|restart|force-reload}" >&2
-	exit 1
-	;;
+        N=/etc/init.d/$NAME
+        # echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
+        log_success_msg "Usage: $N {start|stop|restart|force-reload}"
+        exit 1
+        ;;
 esac
 
 exit 0
+
