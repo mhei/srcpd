43eaf3ae7f612c27791f55f2f660b85fcd2a06ba
gscholz <gscholz@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2007-11-20 22:03:53 +0000

    fixed wrong milli-second format for INFO GA (patch level bug)

--- src/srcp-ga.c
+++ src/srcp-ga.c
@@ -167,7 +167,7 @@ int describeGA(bus_t busnumber, int addr, char *msg)
 
     if ((addr > 0) && (addr <= number_ga)
         && (ga[busnumber].gastate[addr].protocol)) {
-        sprintf(msg, "%ld.%.3ld 101 INFO %ld GA %d %c\n",
+        sprintf(msg, "%lu.%.3lu 101 INFO %ld GA %d %c\n",
                 ga[busnumber].gastate[addr].inittime.tv_sec,
                 ga[busnumber].gastate[addr].inittime.tv_usec / 1000,
                 busnumber, addr, ga[busnumber].gastate[addr].protocol);
@@ -186,7 +186,7 @@ int infoGA(bus_t busnumber, int addr, int port, char *msg)
     if ((addr > 0) && (addr <= number_ga) && (port >= 0) && (port < MAXGAPORT))
         /* && (ga[busnumber].gastate[addr].tv[port].tv_sec>0) ) */
     {
-        sprintf(msg, "%ld.%ld 100 INFO %ld GA %d %d %d\n",
+        sprintf(msg, "%lu.%3lu 100 INFO %ld GA %d %d %d\n",
                 ga[busnumber].gastate[addr].tv[port].tv_sec,
                 ga[busnumber].gastate[addr].tv[port].tv_usec / 1000,
                 busnumber, addr, port, ga[busnumber].gastate[addr].action);
