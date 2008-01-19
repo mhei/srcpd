c75e8d0189f23ebbe8ce5bd0948656373ea04252
rottland <rottland@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2008-01-19 11:36:11 +0000

    PAGE, REG and CVBIT implemented for loopback improved INFO for SM

--- trunk/srcpd/src/loopback.c
+++ trunk/srcpd/src/loopback.c
@@ -58,7 +58,6 @@ int readconfig_LOOPBACK(xmlDocPtr doc, xmlNodePtr node, bus_t busnumber)
                 xmlFree(txt);
             }
         }
-        
         else if (xmlStrcmp(child->name, BAD_CAST "number_gl") == 0) {
             txt = xmlNodeListGetString(doc, child->xmlChildrenNode, 1);
             if (txt != NULL) {
@@ -66,7 +65,6 @@ int readconfig_LOOPBACK(xmlDocPtr doc, xmlNodePtr node, bus_t busnumber)
                 xmlFree(txt);
             }
         }
-        
         else if (xmlStrcmp(child->name, BAD_CAST "number_ga") == 0) {
             txt = xmlNodeListGetString(doc, child->xmlChildrenNode, 1);
             if (txt != NULL) {
@@ -77,25 +75,28 @@ int readconfig_LOOPBACK(xmlDocPtr doc, xmlNodePtr node, bus_t busnumber)
 
         else
             syslog_bus(busnumber, DBG_WARN,
-                    "WARNING, unknown tag found: \"%s\"!\n",
-                    child->name);;
-        
+                       "WARNING, unknown tag found: \"%s\"!\n",
+                       child->name);;
+
         child = child->next;
     }
 
     if (init_GL(busnumber, __loopback->number_gl)) {
         __loopback->number_gl = 0;
-        syslog_bus(busnumber, DBG_ERROR, "Can't create array for locomotives");
+        syslog_bus(busnumber, DBG_ERROR,
+                   "Can't create array for locomotives");
     }
 
     if (init_GA(busnumber, __loopback->number_ga)) {
         __loopback->number_ga = 0;
-        syslog_bus(busnumber, DBG_ERROR, "Can't create array for accessoires");
+        syslog_bus(busnumber, DBG_ERROR,
+                   "Can't create array for accessoires");
     }
 
     if (init_FB(busnumber, __loopback->number_fb)) {
         __loopback->number_fb = 0;
-        syslog_bus(busnumber, DBG_ERROR, "Can't create array for feedback");
+        syslog_bus(busnumber, DBG_ERROR,
+                   "Can't create array for feedback");
     }
     return (1);
 }
@@ -108,11 +109,11 @@ static int init_lineLoopback(bus_t bus)
 /**
  * cacheInitGL: modifies the gl data used to initialize the device
  **/
-int init_gl_LOOPBACK(gl_state_t *gl)
+int init_gl_LOOPBACK(gl_state_t * gl)
 {
     switch (gl->protocol) {
         case 'L':
-        case 'S': /*TODO: implement range checks*/
+        case 'S':              /*TODO: implement range checks */
         case 'P':
             return SRCP_OK;
             break;
@@ -149,7 +150,7 @@ int init_gl_LOOPBACK(gl_state_t *gl)
 /**
  * initGA: modifies the ga data used to initialize the device
  **/
-int init_ga_LOOPBACK(ga_state_t *ga)
+int init_ga_LOOPBACK(ga_state_t * ga)
 {
     if ((ga->protocol == 'M') || (ga->protocol == 'N')
         || (ga->protocol == 'P') || (ga->protocol == 'S'))
@@ -163,11 +164,12 @@ int init_ga_LOOPBACK(ga_state_t *ga)
  */
 int init_bus_LOOPBACK(bus_t i)
 {
-    syslog_bus(i, DBG_INFO, "loopback start initialization (verbosity = %d).",
-        buses[i].debuglevel);
+    syslog_bus(i, DBG_INFO,
+               "loopback start initialization (verbosity = %d).",
+               buses[i].debuglevel);
     if (buses[i].debuglevel == 0) {
         syslog_bus(i, DBG_INFO, "loopback open device %s (not really!).",
-            buses[i].device.file.path);
+                   buses[i].device.file.path);
         buses[i].device.file.fd = init_lineLoopback(i);
     }
     else {
@@ -178,7 +180,7 @@ int init_bus_LOOPBACK(bus_t i)
 }
 
 /*thread cleanup routine for this bus*/
-static void end_bus_thread(bus_thread_t *btd)
+static void end_bus_thread(bus_thread_t * btd)
 {
     int result;
 
@@ -187,15 +189,15 @@ static void end_bus_thread(bus_thread_t *btd)
     result = pthread_mutex_destroy(&buses[btd->bus].transmit_mutex);
     if (result != 0) {
         syslog_bus(btd->bus, DBG_WARN,
-                "pthread_mutex_destroy() failed: %s (errno = %d).",
-                strerror(result), result);
+                   "pthread_mutex_destroy() failed: %s (errno = %d).",
+                   strerror(result), result);
     }
 
     result = pthread_cond_destroy(&buses[btd->bus].transmit_cond);
     if (result != 0) {
         syslog_bus(btd->bus, DBG_WARN,
-                "pthread_mutex_init() failed: %s (errno = %d).",
-                strerror(result), result);
+                   "pthread_mutex_init() failed: %s (errno = %d).",
+                   strerror(result), result);
     }
 
     free(buses[btd->bus].driverdata);
@@ -211,35 +213,38 @@ void *thr_sendrec_LOOPBACK(void *v)
     int addr;
     int last_cancel_state, last_cancel_type;
     int cv[MAX_CV_NUMBER + 1];
+    /* registers 1-4 == CV#1-4; reg5 == CV#29; reg 7-8 == CV#7-8 */
+    int reg6 = 1;
 
-    bus_thread_t* btd = (bus_thread_t*) malloc(sizeof(bus_thread_t));
+    memset(cv, 0, MAX_CV_NUMBER + 1);
+    bus_thread_t *btd = (bus_thread_t *) malloc(sizeof(bus_thread_t));
     if (btd == NULL)
-        pthread_exit((void*) 1);
+        pthread_exit((void *) 1);
     btd->bus = (bus_t) v;
     btd->fd = -1;
 
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &last_cancel_state);
     pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, &last_cancel_type);
 
-    /*register cleanup routine*/
+    /*register cleanup routine */
     pthread_cleanup_push((void *) end_bus_thread, (void *) btd);
 
     syslog_bus(btd->bus, DBG_INFO, "Loopback bus started (device = %s).",
-        buses[btd->bus].device.file.path);
+               buses[btd->bus].device.file.path);
 
-    /*enter endless loop to process work tasks*/
+    /*enter endless loop to process work tasks */
     while (1) {
-        
+
         buses[btd->bus].watchdog = 1;
 
-        /*POWER action arrived*/
+        /*POWER action arrived */
         if (buses[btd->bus].power_changed == 1) {
             buses[btd->bus].power_changed = 0;
             /*
-            char msg[110];
-            infoPower(btd->bus, msg);
-            enqueueInfoMessage(msg);
-            */
+               char msg[110];
+               infoPower(btd->bus, msg);
+               enqueueInfoMessage(msg);
+             */
             buses[btd->bus].watchdog++;
         }
 
@@ -250,7 +255,7 @@ void *thr_sendrec_LOOPBACK(void *v)
             continue;
         }
 
-        /*GL action arrived*/
+        /*GL action arrived */
         if (!queue_GL_isempty(btd->bus)) {
             dequeueNextGL(btd->bus, &gltmp);
             addr = gltmp.id;
@@ -264,7 +269,7 @@ void *thr_sendrec_LOOPBACK(void *v)
             buses[btd->bus].watchdog++;
         }
 
-        /*GA action arrived*/
+        /*GA action arrived */
         if (!queue_GA_isempty(btd->bus)) {
             dequeueNextGA(btd->bus, &gatmp);
             addr = gatmp.id;
@@ -275,30 +280,150 @@ void *thr_sendrec_LOOPBACK(void *v)
             buses[btd->bus].watchdog++;
         }
 
-        /*SM action arrived (process only with power on)*/
+        /*SM action arrived (process only with power on) */
         if (!queue_SM_isempty(btd->bus)) {
             dequeueNextSM(btd->bus, &smtmp);
             session_lock_wait(btd->bus);
-
+            smtmp.value &= 255;
             switch (smtmp.command) {
                 case GET:
-                    if ((smtmp.typeaddr >= 0) &&
-                            (smtmp.typeaddr <= MAX_CV_NUMBER)) {
-                        smtmp.value = cv[smtmp.typeaddr];
+                    smtmp.value = -1;
+                    switch (smtmp.type) {
+                        case REGISTER:
+                            if ((smtmp.typeaddr > 0) &&
+                                (smtmp.typeaddr < 9)) {
+                                if (smtmp.typeaddr < 5
+                                    || smtmp.typeaddr > 6) {
+                                    smtmp.value = cv[smtmp.typeaddr];
+                                    if (smtmp.typeaddr < 5)
+                                        reg6 = 1;
+                                }
+                                else if (smtmp.typeaddr == 5) {
+                                    cv[29] = smtmp.value = cv[29];
+                                }
+                                else {
+                                    smtmp.value = reg6;
+                                }
+                            }
+                            break;
+                        case PAGE:
+                        case CV:
+                            if ((smtmp.typeaddr >= 0) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                smtmp.value = cv[smtmp.typeaddr];
+                            }
+                            break;
+                        case CV_BIT:
+                            if ((smtmp.typeaddr >= 0) && (smtmp.bit >= 0)
+                                && (smtmp.bit < 8) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                smtmp.value = (1 &
+                                               (cv[smtmp.typeaddr] >>
+                                                smtmp.bit));
+                            }
+                            break;
                     }
                     break;
                 case SET:
-                    if ((smtmp.typeaddr >= 0) &&
-                            (smtmp.typeaddr <= MAX_CV_NUMBER)) {
-                        cv[smtmp.typeaddr] = smtmp.value;
+                    switch (smtmp.type) {
+                        case REGISTER:
+                            if ((smtmp.typeaddr > 0) &&
+                                (smtmp.typeaddr < 9)) {
+                                if (smtmp.typeaddr < 5
+                                    || smtmp.typeaddr > 6) {
+                                    if (smtmp.typeaddr < 5)
+                                        reg6 = 1;
+                                    cv[smtmp.typeaddr] = smtmp.value;
+                                }
+                                else if (smtmp.typeaddr == 5) {
+                                    cv[29] = smtmp.value;
+                                }
+                                else {
+                                    reg6 = smtmp.value;
+                                }
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
+                        case PAGE:
+                        case CV:
+                            if ((smtmp.typeaddr >= 0) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                cv[smtmp.typeaddr] = smtmp.value;
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
+                        case CV_BIT:
+                            if ((smtmp.typeaddr >= 0) && (smtmp.bit >= 0)
+                                && (smtmp.bit < 8) && (smtmp.value >= 0) &&
+                                (smtmp.value <= 1) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                if (smtmp.value) {
+                                    cv[smtmp.typeaddr] |= (1 << smtmp.bit);
+                                }
+                                else {
+                                    cv[smtmp.typeaddr] &=
+                                        ~(1 << smtmp.bit);
+                                }
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
                     }
                     break;
                 case VERIFY:
-                    if ((smtmp.typeaddr >= 0) &&
-                            (smtmp.typeaddr <= MAX_CV_NUMBER)) {
-                        if (smtmp.value != cv[smtmp.typeaddr]) {
-                            smtmp.value = 0;
-                        }
+                    switch (smtmp.type) {
+                        case REGISTER:
+                            if ((smtmp.typeaddr > 0) &&
+                                (smtmp.typeaddr < 9)) {
+                                if (smtmp.typeaddr < 5
+                                    || smtmp.typeaddr > 6) {
+                                    if (smtmp.typeaddr < 5)
+                                        reg6 = 1;
+                                    if (smtmp.value != cv[smtmp.typeaddr])
+                                        smtmp.value = -1;
+                                }
+                                else if (smtmp.typeaddr == 5) {
+                                    if (cv[29] != smtmp.value)
+                                        smtmp.value = -1;
+                                }
+                                else {
+                                    if (reg6 != smtmp.value)
+                                        smtmp.value = -1;
+                                }
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
+                        case PAGE:
+                        case CV:
+                            if ((smtmp.typeaddr >= 0) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                if (smtmp.value != cv[smtmp.typeaddr])
+                                    smtmp.value = -1;
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
+                        case CV_BIT:
+                            if ((smtmp.typeaddr >= 0) && (smtmp.bit >= 0)
+                                && (smtmp.bit < 8) &&
+                                (smtmp.typeaddr <= MAX_CV_NUMBER)) {
+                                if (smtmp.value != (1 &
+                                                    (cv[smtmp.typeaddr] >>
+                                                     smtmp.bit)))
+                                    smtmp.value = -1;
+                            }
+                            else {
+                                smtmp.value = -1;
+                            }
+                            break;
                     }
                     break;
             }
@@ -307,7 +432,7 @@ void *thr_sendrec_LOOPBACK(void *v)
             buses[btd->bus].watchdog++;
         }
 
-        /*FB action arrived*/
+        /*FB action arrived */
         /* currently nothing to do here */
         buses[btd->bus].watchdog++;
 
@@ -315,7 +440,7 @@ void *thr_sendrec_LOOPBACK(void *v)
         usleep(1000);
     }
 
-    /*run the cleanup routine*/
+    /*run the cleanup routine */
     pthread_cleanup_pop(1);
     return NULL;
 }
