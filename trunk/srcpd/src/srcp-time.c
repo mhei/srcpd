cd0ea0b6505a7fb3b1f3d54bfd29fdd03271508e
gscholz <gscholz@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2008-10-28 20:52:11 +0000

    comment cleanup

--- trunk/srcpd/src/srcp-time.c
+++ trunk/srcpd/src/srcp-time.c
@@ -17,7 +17,7 @@
 /*local variable for time thread*/
 static pthread_t time_tid;
 
-/*FIXME: this global variable can be accessed by several threads at the
+/*FIXME: this variable can be accessed by several threads at the
  * same time and should be protected by a lock*/
 static vtime_t vtime;
 
