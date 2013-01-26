f909d5428a329ea035d31b3d5863210fa4cdc342
gscholz <gscholz@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2013-01-26 17:07:02 +0000

    streamlining by indent

--- trunk/srcpd/src/hsi-88.h
+++ trunk/srcpd/src/hsi-88.h
@@ -17,21 +17,20 @@
 #ifndef _HSI_88_H
 #define _HSI_88_H
 
-#include <libxml/tree.h> /*xmlDocPtr, xmlNodePtr*/
+#include <libxml/tree.h>        /*xmlDocPtr, xmlNodePtr */
 
 
-typedef struct _HSI_88_DATA
-{
-  int number_fb[3];
-  int refresh;
-  char v_text[50];
+typedef struct _HSI_88_DATA {
+    int number_fb[3];
+    int refresh;
+    char v_text[50];
 } HSI_88_DATA;
 
 int readConfig_HSI_88(xmlDocPtr doc, xmlNodePtr node, bus_t busnumber);
 
 int init_bus_HSI_88(bus_t busnumber);
 int get_bus_config_HSI_88(bus_t busnummer, int *num_gl, int *num_ga,
-                int *num_fb);
-void* thr_sendrec_HSI_88(void*);
+                          int *num_fb);
+void *thr_sendrec_HSI_88(void *);
 
 #endif
