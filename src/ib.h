5e8f29efd901be25005a670ff756df300788b1f6
andre_schenk <andre_schenk@145af907-ac3f-0410-a0cb-9e3d0bbb9ac5>
2013-06-04 13:56:46 +0000

    use enum instead of #define

--- src/ib.h
+++ src/ib.h
@@ -44,116 +44,116 @@ void* thr_sendrec_IB(void *);
 #define SPEED_STEPS 126
 
 /* values for power_state */
-
-#define POWER_OFF 0
-#define POWER_ON  1
+enum {POWER_OFF = 0, POWER_ON = 1};
 
 static const char P50X_DISABLE[] = "xZzA1";
 static const char P50X_ENABLE[] = "ZzA0";
 
 /* general purpose P50Xb commands */
-
-#define XLok        0x80
-#define XLkDisp     0x83
-#define XLokSts     0x84
-#define XLokCfg     0x85
-#define XFunc       0x88
-#define XFuncX      0x89
-#define XFunc34     0x8A
-#define XFuncSts    0x8C
-#define XFuncXSts   0x8D
-#define XFunc34Sts  0x8E
-#define XLimit      0x8F
-#define XTrnt       0x90
-#define XTrntFree   0x93
-#define XTrntSts    0x94
-#define XTrntGrp    0x95
-#define XSensor     0x98
-#define XSensOff    0x99
-#define X88PGet     0x9C
-#define X88PSet     0x9D
-#define Xs88PTim    0x9E
-#define Xs88Cnt     0x9F
-#define XVer        0xA0
-#define XP50XCh     0xA1
-#define XStatus     0xA2
-#define XSOSet      0xA3
-#define XSOGet      0xA4
-#define XHalt       0xA5
-#define XPwrOff     0xA6
-#define XPwrOn      0xA7
-#define XLokoNet    0xC0
-#define XNOP        0xC4
-#define XP50Len1    0xC6
-#define XP50Len2    0xC7
-#define XEvent      0xC8
-#define XEvtLok     0xC9
-#define XEvtTrn     0xCA
-#define XEvtSen     0xCB
-#define XEvtIR      0xCC
-#define XEvtLN      0xCD
-#define XEvtPT      0xCE
-#define XEvtTkR     0xCF
-#define XEvtMem     0xD0
-#define XEvtLSY     0xD1
-#define XEvtBiDi    0xD2
-#define XBiDiSet    0xD3
-#define XBiDiQuery  0xD4
+enum {
+    XLok       = 0x80,
+    XLkDisp    = 0x83,
+    XLokSts    = 0x84,
+    XLokCfg    = 0x85,
+    XFunc      = 0x88,
+    XFuncX     = 0x89,
+    XFunc34    = 0x8A,
+    XFuncSts   = 0x8C,
+    XFuncXSts  = 0x8D,
+    XFunc34Sts = 0x8E,
+    XLimit     = 0x8F,
+    XTrnt      = 0x90,
+    XTrntFree  = 0x93,
+    XTrntSts   = 0x94,
+    XTrntGrp   = 0x95,
+    XSensor    = 0x98,
+    XSensOff   = 0x99,
+    X88PGet    = 0x9C,
+    X88PSet    = 0x9D,
+    Xs88PTim   = 0x9E,
+    Xs88Cnt    = 0x9F,
+    XVer       = 0xA0,
+    XP50XCh    = 0xA1,
+    XStatus    = 0xA2,
+    XSOSet     = 0xA3,
+    XSOGet     = 0xA4,
+    XHalt      = 0xA5,
+    XPwrOff    = 0xA6,
+    XPwrOn     = 0xA7,
+    XLokoNet   = 0xC0,
+    XNOP       = 0xC4,
+    XP50Len1   = 0xC6,
+    XP50Len2   = 0xC7,
+    XEvent     = 0xC8,
+    XEvtLok    = 0xC9,
+    XEvtTrn    = 0xCA,
+    XEvtSen    = 0xCB,
+    XEvtIR     = 0xCC,
+    XEvtLN     = 0xCD,
+    XEvtPT     = 0xCE,
+    XEvtTkR    = 0xCF,
+    XEvtMem    = 0xD0,
+    XEvtLSY    = 0xD1,
+    XEvtBiDi   = 0xD2,
+    XBiDiSet   = 0xD3,
+    XBiDiQuery = 0xD4,
 
 /* programming track P50X commands */
 
-#define XDCC_PDR    0xDA
-#define XDCC_PAR    0xDB
-#define XPT_DCCEWr  0xDC
-#define XPT_FMZEWr  0xDD
-#define XDCC_PD     0xDE
-#define XDCC_PA     0xDF
-#define XPT_Sts     0xE0
-#define XPT_On      0xE1
-#define XPT_Off     0xE2
-#define XPT_SXRd    0xE4
-#define XPT_SXWr    0xE5
-#define XPT_SXSr    0xE6
-#define XPT_FMZSr   0xE7
-#define XPT_FMZWr   0xE8
-#define XPT_MrkSr   0xE9
-#define XPT_DCCSr   0xEA
-#define XPT_DCCQA   0xEB
-#define XPT_DCCRR   0xEC
-#define XPT_DCCWR   0xED
-#define XPT_DCCRP   0xEE
-#define XPT_DCCWP   0xEF
-#define XPT_DCCRD   0xF0
-#define XPT_DCCWD   0xF1
-#define XPT_DCCRB   0xF2
-#define XPT_DCCWB   0xF3
-#define XPT_DCCQD   0xF4
-#define XPT_DCCRL   0xF5
-#define XPT_DCCWL   0xF6
-#define XPT_DCCRA   0xF7
-#define XPT_DCCWA   0xF8
-#define XPT_U750    0xF9
-#define XPT_U755    0xFA
-#define XPT_U760    0xFB
-#define XPT_Src     0xFC
-#define XPT_Ctrl    0xFD
-#define XPT_Term    0xFE
+    XDCC_PDR   = 0xDA,
+    XDCC_PAR   = 0xDB,
+    XPT_DCCEWr = 0xDC,
+    XPT_FMZEWr = 0xDD,
+    XDCC_PD    = 0xDE,
+    XDCC_PA    = 0xDF,
+    XPT_Sts    = 0xE0,
+    XPT_On     = 0xE1,
+    XPT_Off    = 0xE2,
+    XPT_SXRd   = 0xE4,
+    XPT_SXWr   = 0xE5,
+    XPT_SXSr   = 0xE6,
+    XPT_FMZSr  = 0xE7,
+    XPT_FMZWr  = 0xE8,
+    XPT_MrkSr  = 0xE9,
+    XPT_DCCSr  = 0xEA,
+    XPT_DCCQA  = 0xEB,
+    XPT_DCCRR  = 0xEC,
+    XPT_DCCWR  = 0xED,
+    XPT_DCCRP  = 0xEE,
+    XPT_DCCWP  = 0xEF,
+    XPT_DCCRD  = 0xF0,
+    XPT_DCCWD  = 0xF1,
+    XPT_DCCRB  = 0xF2,
+    XPT_DCCWB  = 0xF3,
+    XPT_DCCQD  = 0xF4,
+    XPT_DCCRL  = 0xF5,
+    XPT_DCCWL  = 0xF6,
+    XPT_DCCRA  = 0xF7,
+    XPT_DCCWA  = 0xF8,
+    XPT_U750   = 0xF9,
+    XPT_U755   = 0xFA,
+    XPT_U760   = 0xFB,
+    XPT_Src    = 0xFC,
+    XPT_Ctrl   = 0xFD,
+    XPT_Term   = 0xFE
+};
 
 /* IB error codes */
-
-#define XBADPARAM   0x02
-#define XPWOFF      0x06
-#define XNOTSPC     0x07
-#define XNOLSPC     0x08
-#define XNODATA     0x0A
-#define XNOSLOT     0x0B
-#define XBADLNP     0x0C
-#define XLKBUSY     0x0D
-#define XBADTNP     0x0E
-#define XBADSOV     0x0F
-#define XNOISPC     0x10
-#define XLOWTSP     0x40
-#define XLKHALT     0x41
-#define XLKPOFF     0x42
+enum {
+    XBADPARAM  = 0x02,
+    XPWOFF     = 0x06,
+    XNOTSPC    = 0x07,
+    XNOLSPC    = 0x08,
+    XNODATA    = 0x0A,
+    XNOSLOT    = 0x0B,
+    XBADLNP    = 0x0C,
+    XLKBUSY    = 0x0D,
+    XBADTNP    = 0x0E,
+    XBADSOV    = 0x0F,
+    XNOISPC    = 0x10,
+    XLOWTSP    = 0x40,
+    XLKHALT    = 0x41,
+    XLKPOFF    = 0x42
+};
 
 #endif
